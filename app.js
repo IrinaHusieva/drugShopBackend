94255bd0bd931c1f153992285106741873ad0582
Iryna Husieva <02guseva02@gmail.com>
2024-03-04 10:02:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express from 'express'
 import logger from 'morgan'
 import cors from 'cors'
 import 'dotenv/config'
-// import contactsRouter from './routes/api/contacts.js'
 import shopsRouter from './routes/api/shops.js'
 import {
   handleNotFound,
@@ -12,6 +11,11 @@ import {
 const app = express()
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
+const corsOptions = {
+  origin: ['http://localhost:3000', 'http://192.168.1.45:3000', 'https://IrinaHusieva.github.io'],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 app.use(express.json())
 app.use('/api/shops', shopsRouter);
